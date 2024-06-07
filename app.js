48c7e7ad12c849b2d6c9359bbed030f29c5e5a07
Jyotsna Mehta <53934353+j24m@users.noreply.github.com>
2024-06-08 02:21:04 +0530

    feat : Back button to go back to userInput Modal

--- app.js
+++ app.js
@@ -5,18 +5,23 @@ let userInputModal = document.querySelector(".user-input-container");
 let simulateBtn = document.querySelector(".btn-simulate");
 let toastContainer = document.querySelector(".toast-container");
 let toastMessage = document.querySelector(".toast-message");
-let closeToast = document.querySelector(".btn-close");
-
-simulateBtn.addEventListener("click", generateLiftsAndFloors);
-
+let btnCloseToast = document.querySelector(".btn-close");
+const primaryColor = getComputedStyle(document.documentElement)
+  .getPropertyValue("--primary-color")
+  .trim();
+const secondaryColor = getComputedStyle(document.documentElement)
+  .getPropertyValue("--secondary-color")
+  .trim();
+let backBtn = document.createElement("button");
+// console.log(getComputedStyle(document.documentElement));
 toastContainer.style.display = "none";
 
-closeToast.addEventListener("click", hideToast);
-
+btnCloseToast.addEventListener("click", hideToast);
 function hideToast() {
   toastContainer.style.display = "none";
 }
 
+simulateBtn.addEventListener("click", generateLiftsAndFloors);
 function generateLiftsAndFloors() {
   let floorCount = Number(numberOFloors.value);
   let liftCount = Number(numberOfLifts.value);
@@ -30,5 +35,23 @@ function generateLiftsAndFloors() {
   } else if (liftCount > floorCount) {
     toastContainer.style.display = "flex";
     toastMessage.innerHTML = "Lift count cannot exceed floor count";
+  } else {
+    setTimeout(() => {
+      mainContainer.style.backgroundColor = secondaryColor;
+      userInputModal.style.display = "none";
+      backBtn.style.display = "block";
+      console.log(backBtn);
+      backBtn.classList.add("btn", "btn-floating", "btn-primary", "backBtn");
+      backBtn.title = "Go Back";
+      backBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="M21 11H6.83l3.58-3.59L9 6l-6 6l6 6l1.41-1.41L6.83 13H21z"/></svg>`;
+      mainContainer.appendChild(backBtn);
+    }, 500);
   }
+}
+
+backBtn.addEventListener("click", displayUserInputModal);
+function displayUserInputModal() {
+  mainContainer.style.backgroundColor = primaryColor;
+  userInputModal.style.display = "flex";
+  backBtn.style.display = "none";
 }
